fdde673e5dcb33cdf32d17024df37ad0aad77148
Gustavo Prolla Lacroix <gustavo.prolla-lacroix@grenoble-inp.org>
2026-02-04 12:26:12 +0100

    First version of lp2bc

--- src/BiqCrunch.jl
+++ src/BiqCrunch.jl
@@ -1,10 +1,91 @@
 module BiqCrunch
 
+import MathOptInterface as MOI
+
 # NOTE: Assumes BiqCrunch is installed in the home directory
+#
+#
+function getijval(F, f, n, m)
+	q = ""
+	if F == MOI.ScalarAffineFunction{Float64}
+		for t in f.terms
+			q *= "$m 1 $(t.variable.value) $(n+1) $(t.coefficient/2)\n"
+		end
+	elseif F == MOI.ScalarQuadraticFunction{Float64}
+		for qt in f.quadratic_terms
+			q *= "$m 1 $(qt.variable_1.value) $(qt.variable_2.value) $(qt.coefficient/2)\n"
+		end
+		for at in f.affine_terms
+			q *= "$m 1 $(at.variable.value) $(n+1) $(at.coefficient/2)\n"
+		end
+
+	else
+		error("unsupported function type")
+	end
+	return q
+end
 
 function lp2bc(lp_file::String, bc_file::String)
-	script_path = expanduser("~/BiqCrunch/tools/lp2bc.py")
-	run(pipeline(`python3 $script_path $lp_file`, bc_file))
+	lp_model = MOI.FileFormats.Model(format = MOI.FileFormats.FORMAT_LP)
+	MOI.read_from_file(lp_model, lp_file)
+	output = ""
+
+
+	# TODO: Assert model is binary quadratic.
+
+	# Output variable names
+	vars = MOI.get(lp_model, MOI.ListOfVariableIndices())
+	n = length(vars)
+	var_strings = map(v -> 
+		   "#\t$(v.value): $(MOI.get(lp_model, MOI.VariableName(), v))",
+		   vars)
+	output *= "# List of binary variables:\n$(join(var_strings, "\n"))\n"
+
+	# Output max/min
+	obj_sense = MOI.get(lp_model, MOI.ObjectiveSense())
+	if obj_sense == MOI.MIN_SENSE 
+		output *= "-1 = min problem\n"
+	elseif obj_sense == MOI.MAX_SENSE
+		output *= "1 = max problem\n"
+	else
+		error("feasibility sense not allowed")
+	end
+
+	# Handle constraints
+	rhs = [];
+	Q = "";
+	obj_fun_type = MOI.get(lp_model, MOI.ObjectiveFunctionType())
+	obj_fun = MOI.get(lp_model, MOI.ObjectiveFunction{obj_fun_type}())
+	Q *= getijval(obj_fun_type, obj_fun, n, 0)
+	mi = me = 0
+	supp_funs = [MOI.ScalarQuadraticFunction{Float64}, MOI.ScalarAffineFunction{Float64}]
+	supp_sets = [MOI.LessThan{Float64}, MOI.EqualTo{Float64}, MOI.GreaterThan{Float64}]
+	supp_constraints = Iterators.product(supp_funs, supp_sets)
+	for sc in supp_constraints
+		F, S = sc
+		cis = MOI.get(lp_model, MOI.ListOfConstraintIndices{F, S}())
+		for ci in cis
+			if S == MOI.EqualTo{Float64} me += 1 else mi += 1 end
+			fun = MOI.get(lp_model, MOI.ConstraintFunction(), ci)
+			set = MOI.get(lp_model, MOI.ConstraintSet(), ci)
+			push!(rhs, MOI.constant(set))
+			Q *= getijval(F, fun, n, mi + me)
+			if S == MOI.LessThan{Float64}
+				Q *= "$(mi + me) 2 $mi $mi 1.0\n"
+			elseif S == MOI.GreaterThan{Float64}
+				Q *= "$(mi + me) 2 $mi $mi -1.0\n"
+			end
+		end
+	end
+
+	output *= "$(mi + me) = number of constraints\n"
+	output *= "$(mi == 0 ? 1 : 2) = number of blocks\n"
+	output *= "$(n+1)$(mi > 0 ? ", -$mi" : "")\n"
+	output *= "$(join(rhs, " "))\n"
+	output *= Q
+
+	println(output)
+	write(bc_file, output)
 end
 
 function get_var_mapping(bc_file::String)
